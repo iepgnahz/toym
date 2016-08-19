3bcfeb97928af875df0836951e6a0865bcd55ca5
zhangpei <13488233237@163.com>
2016-08-19 11:41:53 +0800

    add description and inputSteps

--- public/src/components/MenuUpLoadApp.js
+++ public/src/components/MenuUpLoadApp.js
@@ -2,6 +2,7 @@
  * Created by zhangpei on 16/8/18.
  */
 import React, {Component} from 'react';// eslint-disable-line no-unused-vars
+import {connect} from 'react-redux';
 import Logo from '../components/Logo';
 import  AddMaterialInputPackage from '../container/AddMaterialInputPackage';
 import  MaterialListPackage from './MaterialList';
@@ -9,15 +10,38 @@ class MenuUpLoadApp extends Component{
   constructor(props){
     super(props);
   }
+
+  //
   render(){
     return (
-        <div className="container">
+        <div style={{width:'80%',margin:'10px auto',textAlign:'center'}} className="container">
           <Logo />
+          <div>
+            <h4 className="text-left">简介</h4>
+            <textarea className="form-control" rows="2"  style={{overflow: 'auto'}} ref="descriptionInput"
+                      placeholder="简介">
+          </textarea>
+          </div>
+
           <AddMaterialInputPackage />
           <MaterialListPackage />
+          <div>
+            <h4 className="text-left">做法与步骤</h4>
+            <textarea className="form-control" rows="5"  style={{overflow: 'auto'}} ref="stepsInput"
+                      placeholder="为了您的菜谱更好的呈现，您可以:1. xxx 回车 2. xxx 回车 3. xxx .....">
+          </textarea>
+          </div>
+          <button type="button" className="btn btn-info btn-sm" onClick={this. click.bind(this)}>上传菜谱</button>
         </div>
     );
   }
 }
+const mapStateToProps = (state)=> (state);
+const mapDispatchToProps = (dispatch)=> ({
 
-export default MenuUpLoadApp;
+});
+
+var MenuUpLoadAppPackage = connect(mapStateToProps,mapDispatchToProps)(MenuUpLoadApp);
+
+
+export default MenuUpLoadAppPackage;
