03782d64e25967570f2d50a66ae4420dbb90bb02
zhangpei <13488233237@163.com>
2016-08-19 18:30:48 +0800

    modify express

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import regRouters from './server/routers';
+import multer from 'multer';
 import path from 'path';
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve('public/uploads'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({storage: storage});
 
 const app = express();
 
 mongoose.connect('mongodb://localhost/todos');
+
+app.post('/profile', upload.single('avatar'), (req, res, next)=> {
+  res.send({
+    err: null,
+    filePath: 'uploads/' + path.basename(req.file.path)
+  })
+  console.log(req.file.path);
+});
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
---
+++ public/src/components/MenuImage.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import request from 'superagent';
+import {connect} from 'react-redux';
+import {fetchImage} from '../actions'
+class MenuImage extends Component {
+
+  handleClick(file) {
+    let formData = new FormData();
+    formData.append('avatar', file);
+    request.post('/profile')
+        .send(formData)
+        .end((err, res)=> {
+          this.props.fetchImage(res.body.filePath);
+        })
+  }
+
+  isClick() {
+    let input = this.refs.file;
+    let image = this.refs.image;
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        image.src = e.target.result;
+      }
+
+      reader.readAsDataURL(input.files[0]);
+    }
+   this.handleClick(input.files[0]);
+  }
+  render() {
+    return (
+        <div id="wrap" style={{align:'center'}}>
+          <div>
+            <img src="" ref="image" id='img1' style={{height:'200px',width:'300px'}}/>
+          </div>
+          <br/>
+          <br/>
+          <input type="file" ref="file" style={{width:'106px'}} onChange={this.isClick.bind(this)}/>
+        </div>
+    )
+  }
+}
+
+const mapStateToProps=(state)=>(state);
+const mapDispatchToProps=(dispatch)=>({
+  fetchImage: (file)=>{
+    dispatch(fetchImage(file))
+  }
+});
+const MenuImagePackage=connect(mapStateToProps,mapDispatchToProps)(MenuImage);
+export default MenuImagePackage;
