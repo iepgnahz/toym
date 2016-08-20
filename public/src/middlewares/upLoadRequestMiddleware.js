9628d965519935db267aec0e16c33e16f130fa58
yujuan <15809216911@163.com>
2016-08-20 10:14:32 +0800

    middlewear uploadRequestMiddlewear

--- public/src/middlewares/upLoadRequestMiddleware.js
+++ public/src/middlewares/upLoadRequestMiddleware.js
@@ -1,22 +1,19 @@
-/**
- * Created by zhangpei on 16/8/19.
- */
 import request from 'superagent';
 import uploadMenu from '../actions/index';
-const uploadRequestMiddleware = store => next => action => {
+const uploadRequestMiddleware = () => next => action => {
   switch (action.type) {
-    case 'UPLOAD_MENU':
-      request
+  case uploadMenu:
+    request
           .post('/menus')
           .send(action.data)
           .type('form')
           .end((err,res)=>{
             if(res.body){
-              alert('上传失败')
+              alert('上传失败');
             } else {
               alert('上传成功');
             }
-          })
+          });
   }
   next(action);
 };
