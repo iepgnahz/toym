b51c8d7f4be3840636e9a80056c5730cbb12f2ed
ym <942470581@qq.com>
2016-08-20 18:03:43 +0800

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -21,13 +21,15 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/, 
+        test: /\.css$/,
         loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       },
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'} 
+      ,
       {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
     ]
   },
   devtool: "#cheap-source-map",
