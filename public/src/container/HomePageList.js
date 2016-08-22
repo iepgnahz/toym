e71e15fc4d54cb686b67cc99922f036d652552fc
yujuan <15809216911@163.com>
2016-08-22 16:01:56 +0800

    add HomePageList file

--- public/src/container/HomePageList.js
+++ public/src/container/HomePageList.js
@@ -1,3 +1,15 @@
-/**
- * Created by yujuan on 16-8-22.
- */
+import {connect} from 'react-redux';
+import HomePageShowings from '../components/HomePageShowings';
+import {initHomePage} from './../actions/index';
+const mapStateToProps=(state) =>{
+  return state;
+};
+
+const mapDispatchToProps=(dispatch)=>({
+  init_HomePage:()=>{
+    dispatch(initHomePage());
+  }
+});
+const HomePageList=connect(mapStateToProps,mapDispatchToProps)(HomePageShowings);
+
+export default HomePageList;
