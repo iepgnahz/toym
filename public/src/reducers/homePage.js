092b8bddf94d940a70c3b9d1427f4dd22de89524
yujuan <15809216911@163.com>
2016-08-22 16:03:10 +0800

    add homePage file

--- public/src/reducers/homePage.js
+++ public/src/reducers/homePage.js
@@ -1,3 +1,10 @@
-/**
- * Created by yujuan on 16-8-22.
- */
+import {UPLOAD_HOMEPAGE}from './../actions/index';
+
+const homePage=(state=[],action)=>{
+  switch (action.type){
+    case UPLOAD_HOMEPAGE:
+      return action.data;
+  }
+  return state;
+};
+export default homePage;
