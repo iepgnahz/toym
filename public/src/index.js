5565f89e8eb038929ff429c42da7a73e561af60d
yujuan <15809216911@163.com>
2016-08-22 16:07:39 +0800

    add indexRoute

--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react'; // eslint-disable-line no-unused-vars
 import {render} from 'react-dom';
 import App from './components/App';
@@ -8,13 +7,16 @@ import {Provider} from 'react-redux';
 import menuRequestMiddleware from './middlewares/menuRequestMiddleware';
 import menuDetailRequestMiddleware from './middlewares/menuDetailRequestMiddleware';
 import MenuDetailPackage  from './components/MenuDetailApp';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory,IndexRoute } from 'react-router';
 import MenuUpLoadAppPackage from './components/MenuUpLoadApp';
 import uploadRequestMiddleware from './middlewares/upLoadRequestMiddleware';
+import HomePageList from './container/HomePageList';
+import AppRoot from './components/AppRoot';
 
+import homePageMiddleware from './middlewares/homePageMiddleware';
 const store = createStore(
     resultList,
-    applyMiddleware(menuRequestMiddleware,menuDetailRequestMiddleware,uploadRequestMiddleware)
+    applyMiddleware(menuRequestMiddleware, menuDetailRequestMiddleware, uploadRequestMiddleware, homePageMiddleware)
 );
 
 store.dispatch({
@@ -25,10 +27,13 @@ store.dispatch({
 render(
     <Provider store={store}>
       <Router history={browserHistory}>
-        <Route path="/menu" component={App}/>
-        <Route path='/menuDetail/:id' component={MenuDetailPackage}/>
-        <Route path='/menuUpLoad'   component={MenuUpLoadAppPackage}/>
+
+          <Route path="/menu" component={App}/>
+          <Route path='/menuDetail/:id' component={MenuDetailPackage}/>
+          <Route path='/menuUpLoad' component={MenuUpLoadAppPackage}/>
+          <Route path='homePage' component={HomePageList}/>
       </Router>
+      
     </Provider>,
     document.getElementById('app')
 );
