c55e42186bdda3e6d0e84445154f845dfb05938b
wangqi <774948439@qq.com>
2016-08-23 15:34:11 +0000

    add footer

---
+++ public/src/components/Footer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+class Footer extends React.Component{
+  render(){
+    return(
+        <div id="footer">
+          <p>Copyright &copy; 2016  烹然心动PRXD.com 版权所有  |  消费者维权热线：4006067733</p>
+        </div>
+    );
+  }
+}
+export default Footer;
