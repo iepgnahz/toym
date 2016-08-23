61e9f912e3f615288582dd1982023774904bb71d
zhangpei <13488233237@163.com>
2016-08-23 19:45:21 +0800

    add homepage

--- public/src/components/HomePageApp.js
+++ public/src/components/HomePageApp.js
@@ -1,3 +1,16 @@
 /**
  * Created by zhangpei on 16/8/20.
  */
+import React, {Component} from 'react';// eslint-disable-line no-unused-vars
+import Logo from './Logo';
+class HomePageApp extends Component {
+  render() {
+    return (
+        <div>
+          {/*<Logo/>*/}
+        </div>
+    );
+  }
+}
+
+export default HomePageApp;
