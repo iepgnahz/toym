2b5e2fe2dd1821ed591a5a45ad1ae2172738d781
zhangpei <13488233237@163.com>
2016-08-23 17:45:31 +0800

    userCenterRedirPage

--- public/src/reducers/userCenterRedirectPage.js
+++ public/src/reducers/userCenterRedirectPage.js
@@ -1,3 +1,11 @@
 /**
  * Created by zhangpei on 16/8/23.
  */
+var userCenterRedirectPage = function (state=false,action) {
+  switch (action.type){
+    case 'USERCENTERPAGE_REDIRECTED':
+      return action.isJumped;
+  }
+  return false;
+};
+export default userCenterRedirectPage;
