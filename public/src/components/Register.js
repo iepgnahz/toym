451e830c987cf68e1762381e21afcdaf3ba5ca88
zhangpei <13488233237@163.com>
2016-08-23 11:18:01 +0800

    comple the register

--- public/src/components/Register.js
+++ public/src/components/Register.js
@@ -1,3 +1,124 @@
 /**
  * Created by zhangpei on 16/8/21.
  */
+import React, {Component} from 'react'; // eslint-disable-line no-unused-vars
+import {sendMessage}  from '../actions/index';
+import {withRouter} from 'react-router';
+import {connect}  from 'react-redux';
+import {showRegisterErr} from '../actions/index';
+import {sendRegisterMessage} from '../actions/index';
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      errUserMessage:'',
+      errPasswordMessage:'',
+      errTelMessage:''
+    }
+  }
+
+  click() {
+    var username = this.refs.username.value.trim();
+    var password = this.refs.password.value.trim();
+    var passwordAgain = this.refs.passwordAgain.value.trim();
+    var tel = this.refs.tel.value.trim();
+    var  flag = true;
+    if(username.length < 6){
+      this.setState({errUserMessage:'用户名长度不得小于6',
+      });
+      flag = false
+    }
+    if(password.length < 6 || passwordAgain<6 ){
+      this.setState({errPasswordMessage:'密码长度不得小于6',
+      });
+      flag = false
+    }
+    if(password !== passwordAgain){
+      this.setState({errPasswordMessage:'两次输入的密码不一致',
+      });
+      flag = false
+    }
+    if(tel.length === 0){
+      this.setState({
+        errTelMessage:'联系方式不能为空'
+      })
+    }
+    if(flag){
+      this.props.sendRegisterMessage({username:username,password:password,tel:tel})
+    }
+  }
+
+  focusUserName(){
+    this.refs.username.value = '';
+    this.setState({errUserMessage:''})
+    this.props.showRegisterErr('');
+  }
+  focusPassword(){
+    this.setState({errPasswordMessage:''})
+  }
+  focusTel(){
+    this.refs.tel.value = '';
+    this.setState({errTelMessage:''});
+  }
+
+
+
+
+  componentWillUpdate(nextProps) {
+
+    if(nextProps.registerToLogin) {
+      this.props.router.push('/login');
+    }
+  }
+
+  render() {
+    return (
+        <div className=" row col-xs-offset-4 col-xs-4 text-center">
+          <div className="page-header">
+            <h1 className='text-center'> 烹然心动 </h1>
+          </div>
+          <form>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">用户名 : </label>
+              <input type="text" className="form-control" id="exampleInputEmail1" placeholder="用户名" ref="username"
+                     default=""  onFocus={this.focusUserName.bind(this)}/>
+              <p className=" text-danger" >{this.state.errUserMessage}</p>
+              <p className=" text-danger" >{this.props.registerErrShowed}</p>
+            </div>
+            <div className="form-group">
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="密码" ref="password"
+                     default=""  onFocus={this.focusPassword.bind(this)}/>
+              <p className=" text-danger" >{this.state.errPasswordMessage}</p>
+            </div>
+            <div className="form-group">
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="再次确认密码" ref="passwordAgain"
+                     default=""  onFocus={this.focusPassword.bind(this)}/>
+              <p className=" text-danger" >{this.state.errPasswordMessage}</p>
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" id="exampleInputPassword1" placeholder="联系方式" ref="tel"
+                     default=""  onFocus={this.focusTel.bind(this)}/>
+              <p className=" text-danger" >{this.state.errTelMessage}</p>
+            </div>
+            <button type="button" className="btn btn-default" onClick={this.click.bind(this)}>注 册</button>
+          </form>
+        </div>
+    );
+  }
+}
+
+
+var mapStateToProps = (state)=> {
+  return state;
+};
+
+const mapDispatchToProps = (dispatch)=> ({
+  sendRegisterMessage: (data)=> {
+    dispatch(sendRegisterMessage(data));
+  },
+  showRegisterErr: (err)=> {
+    dispatch(showRegisterErr(err));
+  }
+});
+var LoginPackage = connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));
+export default LoginPackage;
