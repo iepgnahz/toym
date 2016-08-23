144c3a2e55839a559164b90ccf51e8b6692a3af3
zhangpei <13488233237@163.com>
2016-08-23 23:11:38 +0800

    insert middleware from request

--- public/src/components/UserIntroduce.js
+++ public/src/components/UserIntroduce.js
@@ -1,3 +1,48 @@
 /**
  * Created by zhangpei on 16/8/23.
  */
+import React, {Component} from 'react';// eslint-disable-line no-unused-vars
+import {connect} from 'react-redux';
+import {getUserMessage} from '../actions/index';
+import {Link} from 'react-router';
+class UserIntroduce extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.getUserCenterMessage(this.props.userId);
+  }
+
+
+  render() {
+    let userMessage = this.props.userDisplayed || {username:'',tel:''}
+    return (
+        <div>
+    <div className="container">
+      <img src="/image/user.jpg" className="col-md-2" style={{height:'170px'}}/>
+      <div className="col-md-2">
+        用户名:<div className="alert alert-info" role="alert" >{userMessage.username}
+        </div>
+        电话号码:<div className="alert alert-info" role="alert">{userMessage.tel}
+        </div>
+      </div>
+      <Link to='/menuUpLoad'>上传菜谱</Link>
+    </div>
+          <hr />
+          <h2 >作品展示</h2>
+          </div>
+    )
+  }
+
+}
+
+const mapStateToProps = (state)=> (state);
+const mapDispatchToProps = (dispatch)=> ({
+  getUserCenterMessage: (userId)=> {
+    dispatch(getUserMessage(userId));
+  }
+});
+
+var UserIntroducePackage = connect(mapStateToProps,mapDispatchToProps)(UserIntroduce);
+export default UserIntroducePackage;
--- public/src/index.js
+++ public/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react'; // eslint-disable-line no-unused-vars
 import {render} from 'react-dom';
 import App from './components/App';
@@ -7,16 +8,26 @@ import {Provider} from 'react-redux';
 import menuRequestMiddleware from './middlewares/menuRequestMiddleware';
 import menuDetailRequestMiddleware from './middlewares/menuDetailRequestMiddleware';
 import MenuDetailPackage  from './components/MenuDetailApp';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory,IndexRoute,Redirect} from 'react-router';
 import MenuUpLoadAppPackage from './components/MenuUpLoadApp';
 import uploadRequestMiddleware from './middlewares/upLoadRequestMiddleware';
-import HomePageList from './container/HomePageList';
+import HomePageApp from './components/HomePageApp';
+import LoginPackage from './components/Login';
+import UserCenterAppPackage from'./components/UserCenterApp';
+import sendMessageRequestMiddleware from './middlewares/sendMessageRequestMiddleware';
+import AppRoot from './components/AppRoot';
+import Register from './components/Register';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import checkUserRequestMiddleware from './middlewares/checkUserRequestMiddleware';
+import Background from './components/Background';
+import getUserMessageMiddleware from './middlewares/getUserMessageMiddleware';
+import userWorksRequestMiddleware from './middlewares/userWorksRequestMiddleware';
 
 
-import homePageMiddleware from './middlewares/homePageMiddleware';
 const store = createStore(
     resultList,
-    applyMiddleware(menuRequestMiddleware, menuDetailRequestMiddleware, uploadRequestMiddleware, homePageMiddleware)
+    applyMiddleware(thunkMiddleware,createLogger(),menuRequestMiddleware,menuDetailRequestMiddleware,uploadRequestMiddleware,sendMessageRequestMiddleware,checkUserRequestMiddleware,getUserMessageMiddleware,userWorksRequestMiddleware)
 );
 
 store.dispatch({
@@ -27,13 +38,18 @@ store.dispatch({
 render(
     <Provider store={store}>
       <Router history={browserHistory}>
-
-          <Route path="/menu" component={App}/>
-          <Route path='/menuDetail/:id' component={MenuDetailPackage}/>
-          <Route path='/menuUpLoad' component={MenuUpLoadAppPackage}/>
-          <Route path='homePage' component={HomePageList}/>
+        <Route path="/" component={AppRoot}>
+          <IndexRoute component={HomePageApp} />
+          <Route path="in" component={Background}>
+            <Route path="login" component={LoginPackage}/>
+            <Route path="register" component={Register}/>
+          </Route>
+          <Route path="menu" component={App}/>
+          <Route path='menuDetail/:id' component={MenuDetailPackage}/>
+          <Route path='menuUpLoad' component={MenuUpLoadAppPackage}/>
+          <Route path="userCenter" component={UserCenterAppPackage}/>
+        </Route>
       </Router>
-      
     </Provider>,
     document.getElementById('app')
 );
