9982230b1e56ed4adaa338e41b8efaaae4c759be
zhangpei <13488233237@163.com>
2016-08-23 11:14:59 +0800

    make a backround for login

--- public/src/components/Background.js
+++ public/src/components/Background.js
@@ -1,3 +1,20 @@
 /**
  * Created by zhangpei on 16/8/22.
  */
+import React, {Component} from 'react';// eslint-disable-line no-unused-vars
+import {connect} from 'react-redux';
+class Background extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>
+    {this.props.children}
+    </div>
+    )
+  }
+}
+
+export default Background
