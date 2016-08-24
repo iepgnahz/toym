46c82a822ec66f1e7a7d22a8f1ec51b906580f26
zhangpei <13488233237@163.com>
2016-08-24 10:56:01 +0800

    add UserMenuItem

--- public/src/components/UserMenuItem.js
+++ public/src/components/UserMenuItem.js
@@ -1,3 +1,35 @@
 /**
  * Created by zhangpei on 16/8/23.
  */
+import React, {Component} from 'react';// eslint-disable-line no-unused-vars
+import {Link} from 'react-router';
+class UserMenuItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    let {name, image, date, description, id} = this.props;
+
+    return (
+        <div className="food-body">
+          <div className="text-center col-sm-6 col-md-4">
+            <div className="thumbnail">
+              <Link to={'/menuDetail/' + id} className="text-center " >
+                <img src={'/'+image} alt="..." style={{height: '180px'}}/>
+              </Link>
+              <div className="caption row">
+                <div className="col-md-6" id="name1">{name}</div>
+                <div className="col-md-6" id="date1">{date}</div>
+              </div>
+              <p id="food-description">{description}</p>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+
+export default UserMenuItem;
