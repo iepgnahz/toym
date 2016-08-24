73fd7fa0c1aace4e56b88cba92ef27fa4462713a
zhangpei <13488233237@163.com>
2016-08-24 10:04:37 +0800

    modify the user route

--- public/src/components/UserCenterApp.js
+++ public/src/components/UserCenterApp.js
@@ -1,13 +1,9 @@
-/**
- * Created by zhangpei on 16/8/21.
- */
 import React, {Component} from 'react';// eslint-disable-line no-unused-vars
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import UserIntroduce  from '../components/UserIntroduce';
 import {getUserCenterMessage} from '../actions/index';
 import UserMenu from './UserMenu';
-import {resetUserCenter} from '../actions/index';
 class UserCenterApp extends Component {
   constructor(props) {
     super(props);
@@ -18,34 +14,30 @@ class UserCenterApp extends Component {
   }
 
   componentWillUpdate(nextProps) {
-    console.log(nextProps.userCenterRedirectPage)
 
-    if(nextProps.userCenterRedirectPage){
-      // this.props.resetUserCenter();
+    if (nextProps.userCenterRedirectPage) {
       this.props.router.push('/in/login');
-
     }
   }
-  render(){
+
+  render() {
     return (
-        (this.props.userCenterMessageLoaded.username )?
         <div className="food-body">
-          <UserIntroduce userId={this.props.userCenterMessageLoaded.username}/>
-          <UserMenu userId={this.props.userCenterMessageLoaded.username}/>
-        </div>:<h1>loading....</h1>
+          <UserIntroduce userId={this.props.params.userId}/>
+          <UserMenu userId={this.props.params.userId}/>
+        </div>
     )
   }
 }
 var mapStateToProps = (state)=> {
+
+
   return state;
 };
 
 const mapDispatchToProps = (dispatch)=> ({
-  getUserCenterMessage: ()=>{
+  getUserCenterMessage: ()=> {
     dispatch(getUserCenterMessage())
-  },
-  resetUserCenter: ()=>{
-    dispatch(resetUserCenter())
   }
 
 });
