07ff801746f5dc5d9f4a87128e6a6236ad178bfb
zhangpei <13488233237@163.com>
2016-08-24 12:01:13 +0800

    modify userMessageLoaded.js

--- public/src/reducers/userMessageLoaded.js
+++ public/src/reducers/userMessageLoaded.js
@@ -1,3 +1,19 @@
 /**
  * Created by zhangpei on 16/8/21.
  */
+const userMessageLoaded = function (state={},action) {
+  switch (action.type){
+    case 'REQUEST_USERMESSAGE':
+      return {
+      isCompleted:false
+      };
+    case "RECEIVE_USERMESSAGE":
+      return {
+        isCompleted:action.isCompleted,
+        username:action.username
+      }
+  }
+  return state
+};
+
+export default  userMessageLoaded;
