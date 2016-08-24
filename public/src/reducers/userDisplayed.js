cd5c5aeb187a794ecc216a6cf50b679b37fde411
zhangpei <13488233237@163.com>
2016-08-24 12:00:28 +0800

    modify userDisplayed

--- public/src/reducers/userDisplayed.js
+++ public/src/reducers/userDisplayed.js
@@ -1,3 +1,12 @@
 /**
  * Created by zhangpei on 16/8/23.
  */
+const userDisplayed=(state={},action)=>{
+  switch(action.type){
+    case 'USER_DISPLAYED':
+      return action.data;
+  }
+  return state;
+};
+
+export default userDisplayed;
