5639537f4b676f6e58b5bb2980f02aa02faeea76
zhangpei <13488233237@163.com>
2016-08-24 11:58:28 +0800

    registerToLogin.js

--- public/src/reducers/registerToLogin.js
+++ public/src/reducers/registerToLogin.js
@@ -1,3 +1,13 @@
 /**
  * Created by zhangpei on 16/8/22.
  */
+var  registerToLogin= function (state=false,action) {
+  switch (action.type){
+    case 'REGISTER_TO_LOGIN':
+      return action.isPromised;
+    case 'RESET_REGISTER':
+      return action.isPromised;
+  }
+  return false;
+};
+export default registerToLogin;
