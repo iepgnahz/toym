eb8d277235e0bf8f6d2a2a04418bbbbb88a824f6
zhangpei <13488233237@163.com>
2016-08-24 11:00:30 +0800

    add registerErrShowed.js

--- public/src/reducers/registerErrShowed.js
+++ public/src/reducers/registerErrShowed.js
@@ -1,3 +1,12 @@
 /**
  * Created by zhangpei on 16/8/22.
  */
+var registerErrShowed = function (state='',action) {
+  switch (action.type){
+    case 'REGISTERERR_SHOWED':
+      return action.err
+  }
+ return state
+};
+
+export default registerErrShowed;
