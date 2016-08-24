4c9e9436bfb7aa41ca95097bb9c1158d6af0ed38
zhangpei <13488233237@163.com>
2016-08-24 10:09:17 +0800

    add UserMenu Message

--- public/src/components/UserMenu.js
+++ public/src/components/UserMenu.js
@@ -1,3 +1,37 @@
-/**
- * Created by zhangpei on 16/8/23.
- */
+import React ,{Component} from 'react';
+import {connect} from 'react-redux'
+import {getUserWorks} from '../actions/index'
+import UserMenuItem from './UserMenuItem';
+class UserMenu extends Component {
+  componentDidMount() {
+    console.log('aaa')
+    this.props.getUserWorks(this.props.userId);
+  }
+  render() {
+    let userMenu = this.props.selfWorksGetted || [];
+    return (
+        <div className="row">
+          {
+            userMenu.map((v, k)=> (
+                <UserMenuItem id={v._id} key={k} name={v.name} image={v.image} date={v.date} description={v.description}
+                          />
+            ))
+          }
+        </div>
+
+    );
+  }
+}
+
+
+const mapStateToProps = (state)=> (state);
+const mapDispatchToProps = (dispatch)=> ({
+  getUserWorks: (userId)=> {
+    dispatch(getUserWorks(userId));
+  }
+});
+
+var UserMenuPackage = connect(mapStateToProps,mapDispatchToProps)(UserMenu);
+
+
+export default UserMenuPackage;
