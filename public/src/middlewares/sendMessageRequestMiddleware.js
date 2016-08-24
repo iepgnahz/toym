af654f1feef67f833a57500aeabe8c1dd4d7b9af
zhangpei <13488233237@163.com>
2016-08-24 08:29:05 +0800

    get a midlleware to send request

--- public/src/middlewares/sendMessageRequestMiddleware.js
+++ public/src/middlewares/sendMessageRequestMiddleware.js
@@ -1,3 +1,43 @@
 /**
  * Created by zhangpei on 16/8/21.
  */
+import request from 'superagent';
+const sendMessageRequestMiddleware = store => next => action =>{
+  switch (action.type) {
+    case 'MESSAGE_SENDED':
+      request
+          .post('/menus/loginTest')
+          .send(action.data)
+          .type('json')
+          .end((err,res)=>{
+            // if(res.text === '该用户不存在'){
+            //   alert(res.text)
+            // } else if(res.text === '密码不正确'){
+            //   alert(res.text)
+            // } else {
+            //   store.dispatch({
+            //     type:'USER_JUMPED',
+            //     isJumped:true
+            //   })
+            // }
+            if(res.status === 501){
+              store.dispatch({
+                type: 'LOGINUSERERR_SHOWED',
+                err: res.body.err
+              })
+            }
+            if(res.ok && !err){
+              store.dispatch({
+                type:'USER_JUMPED',
+                data: {
+                  isJumped: true,
+                  username: res.body.data
+                }//图上假数据
+              })
+            }
+          })
+  }
+  next(action);
+};
+
+export default sendMessageRequestMiddleware;
