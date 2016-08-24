bb28e0996839cbcd9e3db76c337d4aee1f40063f
zhangpei <13488233237@163.com>
2016-08-24 10:58:26 +0800

    add redirectPage.js

--- public/src/reducers/redirectPage.js
+++ public/src/reducers/redirectPage.js
@@ -1,3 +1,11 @@
 /**
  * Created by zhangpei on 16/8/21.
  */
+var redirectPage = function (state=false,action) {
+  switch (action.type){
+    case 'PAGE_REDIRECTED':
+      return action.isJumped;
+  }
+  return false;
+};
+export default redirectPage;
