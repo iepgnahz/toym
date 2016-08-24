193956ff5580d8a745ba2fcdee365dc6077286d8
zhangpei <13488233237@163.com>
2016-08-24 12:02:49 +0800

    modify Sessions.js

--- server/models/Sessions.js
+++ server/models/Sessions.js
@@ -1,3 +1,12 @@
 /**
  * Created by zhangpei on 16/8/22.
  */
+import mongoose from 'mongoose';
+
+var Sessions = mongoose.model('Sessions',{
+  username:String,
+  password:String,
+  randomId:String
+});
+
+export default Sessions;
