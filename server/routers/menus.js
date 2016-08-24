f8c14b36b1cea475bdde51fe8af923f508c7b4ff
zhangpei <13488233237@163.com>
2016-08-24 12:03:45 +0800

    modify menus.js

--- server/routers/menus.js
+++ server/routers/menus.js
@@ -1,10 +1,70 @@
 import express from 'express';
 import Menus from '../models/Menus';
-
+import Users from '../models/Users';
+var cookieParser = require('cookie-parser');  //
+// import session from 'express-session';
+import uuid from 'node-uuid';
+import Session from '../models/Sessions';
 let router = express.Router();
-//查所有数据
+
+router.use(cookieParser());
+
+router.get('/confirmUpload',(req,res)=>{
+  if(req.cookies.user){
+    Session.findOne({randomId:req.cookies.user},(err,doc)=>{
+      if(err){
+        res.status(403).send('');
+      }else {
+        res.status(200).send(doc.username)
+      }
+    })
+  } else {
+    res.status(403).send('');
+  }
+});
+
+
+router.get('/confirmUserCenter',(req,res)=>{
+
+  if(req.cookies.user){
+    Session.findOne({randomId:req.cookies.user},(err,doc)=>{
+      if(err){
+
+        res.status(403);
+      }else {
+        res.status(200).send(doc.username)
+      }
+    })
+  } else {
+    res.status(403);
+  }
+});
+
+router.get(`/userWorks/:userId`,(req,res)=>{
+  Menus.find({user:req.params.userId},(err,doc)=>{
+      if(doc){
+        res.status(200).send(doc)
+      } else {
+        res.status(404)
+      }
+  });
+});
+
+
+router.get('/user/:userId',(req,res)=>{
+  Users.findOne({username:req.params.userId},(err,doc)=>{
+    if(doc){
+      res.status(200).send(doc)
+    }
+    if(err){
+      res.status(400)
+    }
+  })
+});
 
 router.get('/', (req, res)=> {
+  Menus.find({}, {},{limit:9}, (err, data)=> {
+    res.status(200).send(data);
   Menus.find({}).sort({_id:-1}).limit(9).exec(function(err, data) {
     res.send(data);
   });
@@ -15,21 +75,69 @@ router.get('/homePage',(req,res) =>{
   });
 });
 
-router.get('/:id', (req, res)=> {
-  Menus.findOne({_id: req.params.id}, (err, doc)=> {
-    res.send(doc);
-  });
+
+
+
+
+
+router.post('/registerCheck',(req,res)=>{
+  Users.findOne({username:req.body.username},(err,doc)=>{
+    if(doc){
+      res.status(501).send({err:'用户名重复'})
+    } else {
+      new Users({username:req.body.username,password:req.body.password,tel:req.body.tel}).save((err,doc)=>{
+        res.status(200).send({data:'success'})
+      })
+    }
+    if(err){
+      res.status(501)
+    }
+  })
+});
+
+
+
+router.post('/loginTest',(req,res)=>{
+  Users.findOne({username:req.body.username},(err,doc)=>{
+    if(!doc){
+      res.status(501).send({err:'该用户不存在'})
+    } else if(doc.password === req.body.password){
+      var randomId = uuid.v4();
+       new Session( {
+        username:req.body.username,
+        password:req.body.password,
+        randomId:randomId //
+      }).save((err,doc)=>{
+         res.cookie('user',randomId,{path:'/'});
+         res.status(200).send({data:req.body.username});
+       });
+    } else {
+      res.status(501).send({err:'密码不正确'})
+    }
+    if(err){
+      res.status(501).send({err:'数据库操作有误'})
+    }
+  })
 });
 
 router.post('/',(req,res)=>{
   new Menus(req.body).save((err)=>{
     if(err) {
-      res.send('fail');
+      res.status(404).send('fail');
     } else {
-      res.send('success');
+      res.status(200).send('success');
     }
   });
 });
+
+router.get('/:id', (req, res)=> {
+  Menus.findOne({_id: req.params.id}, (err, doc)=> {
+    res.status(200).send(doc);
+  });
+});
+
+
+
 
 module.exports = router;
 // export default router;
