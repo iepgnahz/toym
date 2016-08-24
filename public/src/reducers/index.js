1528189c9c5b27973b9345f4e179c3d3664d43ae
zhangpei <13488233237@163.com>
2016-08-24 08:29:52 +0800

    add a action for send message

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -4,6 +4,16 @@ import menuItem from './menuItem';
 import materialUploaded from './materialUploaded';
 import loadImage from './uploadImage';
 import resultJump from './resultJump';
+import loginJumped from './loginJumped';
+import userMessageLoaded from './userMessageLoaded';
+import redirectPage from './redirectPage';
+import registerToLogin from './registerToLogin';
+import registerErrShowed from './registerErrShowed';
+import loginUserErrShowed from './loginUserErrShowed';
+import userDisplayed from './userDisplayed';
+import userCenterRedirectPage from './userCenterRedirectPage';
+import userCenterMessageLoaded from './userCenterMessageLoaded';
+import selfWorksGetted from './selfWorksGetted';
 import homePage from './homePage';
 const resultList = combineReducers({
   menu,
@@ -11,6 +21,16 @@ const resultList = combineReducers({
   materialUploaded,
   loadImage,
   resultJump,
+  loginJumped,
+  userMessageLoaded,
+  redirectPage,
+  registerToLogin,
+  registerErrShowed,
+  loginUserErrShowed,
+  userDisplayed,
+  userCenterRedirectPage,
+  userCenterMessageLoaded,
+  selfWorksGetted,
   homePage
 });
 
