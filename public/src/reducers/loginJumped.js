73cbf5a6472b3f1c6af3acc0b0da88890aa9fb90
zhangpei <13488233237@163.com>
2016-08-24 08:30:40 +0800

    add a reducer for send message

--- public/src/reducers/loginJumped.js
+++ public/src/reducers/loginJumped.js
@@ -1,3 +1,11 @@
-/**
- * Created by zhangpei on 16/8/21.
- */
+
+var loginJumped = function (state={},action) {
+  switch (action.type){
+    case 'USER_JUMPED':
+      return action.data;
+    case 'RESET_STATE':
+      return action.data;
+  }
+  return state;
+};
+export default loginJumped;
