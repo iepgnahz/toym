2161592253d5cc5904e8eed9f3c99b69756c91a5
zhangpei <13488233237@163.com>
2016-08-24 12:03:16 +0800

    modify Users.js

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,3 +1,12 @@
 /**
  * Created by zhangpei on 16/8/20.
  */
+import mongoose from 'mongoose';
+
+var Users = mongoose.model('Users',{
+  username:String,
+  password:String,
+  tel:String
+});
+
+export default Users;
